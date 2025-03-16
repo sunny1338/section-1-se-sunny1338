d7da08d099eefdf4e8d76a5c74d1ab464eb1b01f
sunny1338 <sunny322316@gmail.com>
2025-03-16 14:24:23 +0800

    chore: variable names

--- CleanCodeHW/calculate_pi.py
+++ CleanCodeHW/calculate_pi.py
@@ -10,13 +10,13 @@ POINT_COUNT_INCREMENT = 1
 inside_circle = 0
 
 # Randomly generate points and count those inside the circle
-for _ in range(NUM_POINTS):
+for num_point in range(NUM_POINTS):
     x = random.uniform(-RADIUS, RADIUS)
-    y = random.uniform(-1, 1)
-    if x**SQUARE_EXPONENT + y**2 <= RADIUS**SQUARE_EXPONENT:
+    y = random.uniform(-RADIUS, RADIUS)
+    if x**SQUARE_EXPONENT + y**SQUARE_EXPONENT <= RADIUS**SQUARE_EXPONENT:
         inside_circle += POINT_COUNT_INCREMENT
 
 # Estimate pi based on the number of points inside the circle
-pi_neapple = (inside_circle / NUM_POINTS) * AREA_FACTOR
+pi = (inside_circle / NUM_POINTS) * AREA_FACTOR
 
-print(f"Estimated value of pi is: {pi_neapple}")
+print(f"Estimated value of pi is: {pi}")
